d35e49c8e474c4235c732e04ae441cb1e9989f92
Rocha <jrocha@proforma.com>
2018-08-01 13:59:02 -0400

    Getting_closer

--- rockpaperscissors/public/main.js
+++ rockpaperscissors/public/main.js
@@ -8,11 +8,12 @@ const buttonClickEvent = () => {
 
   console.log(player1choice)
 
+
   newValue = document.querySelector('.rock').textContent
 
   document.querySelector('.output1').textContent = newValue
 
-  player1choice= newValue
+  player1choice = newValue
 
 }
 
@@ -20,19 +21,19 @@ document.querySelector('.rock').addEventListener('click', buttonClickEvent)
 
 
 // //Player 1 Paper
-let player1choice2
+
 
 const button2ClickEvent = () => {
   console.log('button was click')
   let currentValue = document.querySelector('.output1').textContent
 
-  console.log(player1choice2)
+  console.log(player1choice)
 
   newValue = document.querySelector('.paper').textContent
 
   document.querySelector('.output1').textContent = newValue
 
-  player1choice= newValue
+  player1choice = newValue
 
 }
 
@@ -42,20 +43,19 @@ document.querySelector('.paper').addEventListener('click', button2ClickEvent)
 //
 //
 // //Player 1 Scissors
-//
-let player1choice3
+
 
 const button3ClickEvent = () => {
   console.log('button was click')
   let currentValue = document.querySelector('.output1').textContent
 
-  console.log(player1choice3)
+  console.log(player1choice)
 
   newValue = document.querySelector('.scissors').textContent
 
   document.querySelector('.output1').textContent = newValue
 
-  player1choice3= newValue
+  player1choice = newValue
 
 }
 
@@ -79,11 +79,64 @@ const p2buttonClickEvent = () => {
 
   document.querySelector('.output2').textContent = newValue
 
-  player2choice= newValue
+  player2choice = newValue
+
+}
+
+// //Player 2 Paper
+
+const p2buttonClickEvent2 = () => {
+  console.log('button was click')
+  let currentValue = document.querySelector('.output2').textContent
+
+  console.log(player2choice)
+
+  newValue = document.querySelector('.paper2').textContent
+
+  document.querySelector('.output2').textContent = newValue
+
+  player2choice = newValue
+
+  //determine who won
+  if (player1choice === "Rock"){
+    console.log("player 2 won!")
+  }
+
+}
+
+// //Player 2 scissors
+//
+
+const p2buttonClickEvent3 = () => {
+  console.log('button was click')
+  let currentValue = document.querySelector('.output2').textContent
+
+  console.log(player2choice)
+
+  newValue = document.querySelector('.scissors2').textContent
+
+  document.querySelector('.output2').textContent = newValue
+
+  player2choice = newValue
+
+  //determine who won
+  if (player1choice === "Rock"){
+    console.log('player 2 lost')
+  }
+
+  if (player1choice === "Paper") {
+    console.log('player 2 won!')
+  }
+
+  if (player1choice === "Scissors") {
+    console.log('Tie!')
+  }
 
 }
 
 document.querySelector('.rock2').addEventListener('click', p2buttonClickEvent)
+document.querySelector('.paper2').addEventListener('click', p2buttonClickEvent2)
+document.querySelector('.scissors2').addEventListener('click', p2buttonClickEvent3)
 //
 //
 //
@@ -92,45 +145,45 @@ document.querySelector('.rock2').addEventListener('click', p2buttonClickEvent)
 //
 // //Player 2 Paper
 //
-let player2choice2
-
-const p2buttonClickEvent2 = () => {
-  console.log('button was click')
-  let currentValue = document.querySelector('.output2').textContent
-
-  console.log(player2choice2)
-
-  newValue = document.querySelector('.paper2').textContent
-
-  document.querySelector('.output2').textContent = newValue
-
-  player2choice2= newValue
-
-}
-
-document.querySelector('.paper2').addEventListener('click', p2buttonClickEvent2)
+// let player2choice2
 //
+// const p2buttonClickEvent2 = () => {
+//   console.log('button was click')
+//   let currentValue = document.querySelector('.output2').textContent
 //
+//   console.log(player2choice2)
 //
+//   newValue = document.querySelector('.paper2').textContent
 //
+//   document.querySelector('.output2').textContent = newValue
 //
+//   player2choice2= newValue
 //
-// //Player 2 scissors
+// }
 //
-let player2choice3
-
-const p2buttonClickEvent3 = () => {
-  console.log('button was click')
-  let currentValue = document.querySelector('.output2').textContent
-
-  console.log(player2choice3)
-
-  newValue = document.querySelector('.scissors2').textContent
-
-  document.querySelector('.output2').textContent = newValue
-
-  player2choice3= newValue
-
-}
-
-document.querySelector('.scissors2').addEventListener('click', p2buttonClickEvent3)
+// document.querySelector('.paper2').addEventListener('click', p2buttonClickEvent2)
+// //
+// //
+// //
+// //
+// //
+// //
+// // //Player 2 scissors
+// //
+// let player2choice3
+//
+// const p2buttonClickEvent3 = () => {
+//   console.log('button was click')
+//   let currentValue = document.querySelector('.output2').textContent
+//
+//   console.log(player2choice3)
+//
+//   newValue = document.querySelector('.scissors2').textContent
+//
+//   document.querySelector('.output2').textContent = newValue
+//
+//   player2choice3= newValue
+//
+// }
+//
+// document.querySelector('.scissors2').addEventListener('click', p2buttonClickEvent3)
